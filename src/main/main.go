f61172002408267904b85268da9115e8581203e6
Administrator <admin@example.com>
2019-11-27 15:49:10 +0800

    go 里面的函数判断

--- src/main/main.go
+++ src/main/main.go
@@ -5,8 +5,13 @@ import (
 )
 
 func main() {
+	const constVar float32 = 3.1415926
 	var count int = 10
 
-	fmt.Printf("开始")
-	print(count)
+	fmt.Println("开始")
+	println(count)
+	fmt.Println("打印常量的值:  ", constVar)
+	if constVar > 2 {
+		fmt.Println("常量比2大")
+	}
 }
