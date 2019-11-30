01111769443937ef8fde61cb13047057acc0eca9
Administrator <admin@example.com>
2019-11-30 16:29:15 +0800

    声明变量时指定设备类型

--- src/main/varMain.go
+++ src/main/varMain.go
@@ -2,8 +2,26 @@ package main
 
 import "fmt"
 
-func main() {
+func variableFuncV1() {
+	var age int
+	fmt.Println(" my age is :", age)
+}
 
-	var age int //变量声明
-	fmt.Println("my age is : ", age)
+func variableFuncV2() {
+	//为变量赋值
+	var gender string
+	gender = "男"
+	fmt.Println("性别是: ", gender)
+}
+
+func variableFuncV3() {
+	//声明变量时候，指明初始值
+	var height int = 32
+	fmt.Println(" 身高是:  ", height)
+}
+
+func main() {
+	variableFuncV1()
+	variableFuncV2()
+	variableFuncV3()
 }
