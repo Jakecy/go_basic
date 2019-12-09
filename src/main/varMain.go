879a56b93afea8611414de26eb07102d6285a90e
Administrator <admin@example.com>
2019-12-09 15:59:19 +0800

    Go-常量

--- src/main/varMain.go
+++ src/main/varMain.go
@@ -43,5 +43,9 @@ func varDeclarationShortHand() {
 }
 
 func main() {
-	varDeclarationShortHand()
+	var i = 5
+	var f = 5.6
+	var c = 5 + 6i
+	fmt.Printf("i's type %T, f's type %T, c's type %T", i, f, c)
+
 }
