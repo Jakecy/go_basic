893f7f0dd9aa165c0fc4267941e27aa1c3fe68d3
Administrator <admin@example.com>
2019-12-11 11:54:26 +0800

    Go教程第七篇: 包

--- src/geometry/rectangle/rectprops.go
+++ src/geometry/rectangle/rectprops.go
@@ -1,1 +1,13 @@
 package rectangle
+
+import "math"
+
+func Area(len, wid float64) float64 {
+	area := len * wid
+	return area
+}
+
+func Diagonal(len, wid float64) float64 {
+	diagonal := math.Sqrt((len * len) + (wid * wid))
+	return diagonal
+}
