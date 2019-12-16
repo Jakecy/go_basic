633bb16b01e459d87548680b16c895ff45571f74
Administrator <admin@example.com>
2019-12-16 10:22:20 +0800

    数组&&slice&&可变参数函数

--- src/array/ArraySlice.go
+++ src/array/ArraySlice.go
@@ -7,7 +7,44 @@ func main() {
 	//以short hand创建数组并赋值
 	//declareArrayUsingShortHand()
 	//使用三个点...创建消息
-	declareArrayUsing3Dots()
+	//declareArrayUsing3Dots()
+	//获取数组长度
+	//getArrayLegth()
+	//使用for循环遍历数组
+	//literateArrayUsingForLoop()
+	//使用range遍历数组
+	literateArrayUsingRange()
+
+}
+
+func literateArrayUsingRange() {
+	sum := 0
+	var arr = [...]int{23, 25, 78, 36, 88, 520}
+	for i, v := range arr {
+		fmt.Println("每次遍历的：i 是,v 是： ", i, v)
+		sum += v
+	}
+}
+
+func literateArrayUsingForLoop() {
+	var arr = [...]int{23, 25, 78, 36, 88, 520}
+	//遍历
+	for i := 0; i < len(arr); i++ {
+		fmt.Println(" 当前的数组元素是: ", arr[i])
+	}
+}
+
+func getArrayLegth() {
+	//创建一个float64数组
+	var arr = [...]float64{23.25, 78.36, 88.520}
+	fmt.Println("数组arr的长度是: ", len(arr))
+}
+
+func arraySizeIsType() {
+	/*	a :=[...]int{12,18,19}
+
+		var b [5]int
+		b=a*/
 }
 
 func declareArrayUsing3Dots() {
